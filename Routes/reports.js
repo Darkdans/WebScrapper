5b375b8fb57658f4dbd09cddb1f22ff99d8337d2
Darkdans <dans.15@hotmail.com>
2017-12-20 02:25:34 -0600

    Subiendo actualizacion chingonsisima

--- Routes/reports.js
+++ Routes/reports.js
@@ -21,11 +21,11 @@ var AccionSchema = mongoose.Schema({
 });
  
 var Accion = mongoose.model('accion', AccionSchema, 'accion');
-var Comedia  = mongoose.model('comedia',  Schema, 'comedia');
-var Drama  = mongoose.model('drama',  Schema, 'drama');
-var Horror  = mongoose.model('horror ',  Schema, 'horror');
-var Mystery  = mongoose.model('mystery ',  Schema, 'mystery');
-var Thriller  = mongoose.model('thriller ',  Schema, 'thriller');
+var Comedia  = mongoose.model('comedia',  ComediaSchema, 'comedia');
+var Drama  = mongoose.model('drama',  DramaSchema, 'drama');
+var Horror  = mongoose.model('horror ',  HorrorSchema, 'horror');
+var Mystery  = mongoose.model('mystery ', MysterySchema, 'mystery');
+var Thriller  = mongoose.model('thriller ',  ThrillerSchema, 'thriller');
 
 router.route('/accion').get(function(req, res){
 	Accion.find({}, function(err, Accion){
@@ -43,4 +43,33 @@ router.route('/comedia').get(function(req, res){
 	})
 })
 
+router.route('/drama').get(function(req, res){
+	Drama.find({}, function(err, Drama){
+		if(err) return console.error(err);
+		res.json(Drama);
+		console.log(Drama);
+	})
+})
+router.route('/horror').get(function(req, res){
+	Horror.find({}, function(err, Horror){
+		if(err) return console.error(err);
+		res.json(Horror);
+		console.log(Horror);
+	})
+})
+router.route('/mystery').get(function(req, res){
+	Mystery.find({}, function(err, Mystery){
+		if(err) return console.error(err);
+		res.json(Mystery);
+		console.log(Mystery);
+	})
+})
+router.route('/thriller').get(function(req, res){
+	Thriller.find({}, function(err, Thriller){
+		if(err) return console.error(err);
+		res.json(Thriller);
+		console.log(Thriller);
+	})
+})
+ 
 module.exports = router; 
