415b8cbfbb1e5f803614188f0c1f13c92d5edcb2
Darkdans <dans.15@hotmail.com>
2017-12-20 02:05:46 -0600

    Subiendo actualizacion 1

--- Routes/reports.js
+++ Routes/reports.js
@@ -13,16 +13,13 @@ db.once('open', function(){
 
 //Seccion de Accion//
 var AccionSchema = mongoose.Schema({
-	title: String,
-	image: String,
-	release: String,
-	description: String
+	title: String
 });
  
 var Accion = mongoose.model('accion', AccionSchema, 'accion');
 
 router.route('/accion').get(function(req, res){
-	Accion.find({}, 'title', function(err, Accion){
+	Accion.find({}, , function(err, Accion){
 		if(err) return console.error(err);
 		res.json(Accion);
 		console.log(Accion);
@@ -43,75 +40,5 @@ router.route('/comedia').get(function(req, res){
 		console.log(Comedia);
 	})
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-var PeliculasSchema = mongoose.Schema({
-	pelicula: String
-});
-
-var Pelicula = mongoose.model('Pelicula', PeliculasSchema);
-
-router.route('/peliculas').get(function(req, res){
-	Cine.find({}, 'titulo', function(err, cines){
-		if(err) return console.error(err);
-		res.json(cines);
-		console.log(cines);
-	})
-})
-
-router.route('/peliculas/:peliculaID').get(function(req, res){
-	console.log(req.params.peliculaID)
-	Cine.findById(req.params.peliculaID, function(err, cines){
-		if(err) return console.error(err);
-		res.json(cines);
-		console.log(cines);
-	})
-})
-
-var PerroSchema = mongoose.Schema({
-	name: String,
-	raza: String,
-	edad: String
-});
-var Perro = mongoose.model('Perro', PerroSchema);
-router.route('/perros').get(function(req, res){
-	console.log('si se metio');
-	Perro.find(function(err, perros){
-	if(err) return console.error(err);
-	res.json(perros);
-	console.log(perros);
-})
-})
-router.route('/reports').get(function(req, res){
-	var content;
-			fs.readFile('./Routes/output.json', 'utf-8', function(err, data){
-				content = data;
-		 	console.log('File successfuly read!');
-			res.json(content);
-		});
-});
-router.route('/reports/:id').get(function(req, res){
-	res.json({id: 0});
-})*/
-
-
 
 module.exports = router; 
