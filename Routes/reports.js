663259713e85cfa915d2891fa756f803dc84b63d
Darkdans <dans.15@hotmail.com>
2017-12-20 01:48:26 -0600

    Subiendo ahora 2 rutas en vez de una

--- Routes/reports.js
+++ Routes/reports.js
@@ -1,4 +1,4 @@
-
+//Declaración de librerias//
 var Report = require('./../Modelos/report');
 var express = require('express');
 var mongoose = require('mongoose');
@@ -6,12 +6,12 @@ var fs = require('fs');
 mongoose.connect('mongodb://darkdans:3212333222d@ds245755.mlab.com:45755/moviesapi', {useMongoClient: true });
 
 var router = express.Router();
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error;'));
 db.once('open', function(){
 });
 
+//Seccion de Accion//
 var AccionSchema = mongoose.Schema({
 	title: String
 });
@@ -25,6 +25,39 @@ router.route('/accion').get(function(req, res){
 		console.log(Accion);
 	})
 })
+
+//Seccion de comedia//
+var ComediaSchema = mongoose.Schema({
+	title: String
+});
+ 
+var Comedia = mongoose.model('comedia', ComediaSchema, 'comedia');
+
+router.route('/comedia').get(function(req, res){
+	Comedia.find({}, 'title', function(err, Comedia){
+		if(err) return console.error(err);
+		res.json(Comedia);
+		console.log(Comedia);
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 var PeliculasSchema = mongoose.Schema({
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+//Declaración de librerias//
 var express = require('express');
 var bodyParser = require('body-parser');
 var mogoose = require('mongoose');
 var reports = require('./Routes/reports');
-
 var app = express();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use('/apu', reports);
+app.use('/api', reports);
+
 
 var port  = process.env.PORT || 3000;
 
@@ -17,13 +18,7 @@ app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Authorization,Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
-/*
-var router = express.Router();
-router.get("/", function(req, res){
-	res.json({message: 'Hi everybody...!'})
-});
-app.use('/api', router);
-*/
+ 
 
 app.listen(port);
 console.log('Server listening in the port: ' +port);
